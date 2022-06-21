4f2e86843b76781f338a5bbcb50051131e561938
Kuria <012nkuria@gmail.com>
2022-06-21 19:15:29 +0200

    Update alert_windows.go

--- alert_windows.go
+++ alert_windows.go
@@ -8,14 +8,14 @@ import (
 )
 
 // Alert displays a desktop notification and plays a default system sound.
-func Alert(title, message, appIcon string) error {
+func Alert(appName, title, message, appIcon string) error {
 	if isWindows10 {
-		note := toastNotification(title, message, pathAbs(appIcon))
+		note := toastNotification(appName, title, message, pathAbs(appIcon))
 		note.Audio = toast.Default
 		return note.Push()
 	}
 
-	if err := Notify(title, message, appIcon); err != nil {
+	if err := Notify(appName, title, message, appIcon); err != nil {
 		return err
 	}
 	return Beep(DefaultFreq, DefaultDuration)
